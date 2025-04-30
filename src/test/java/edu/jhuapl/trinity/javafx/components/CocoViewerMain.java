5626ac6227afb77c3235d7b404ed19725fc1a4e1
Sean Phillips <sean.mi.phillips@gmail.com>
2025-04-30 08:14:05 -0400

    Test app for new Coco Annotations viewer

---
+++ src/test/java/edu/jhuapl/trinity/javafx/components/CocoViewerMain.java
@@ -0,0 +1,15 @@
+package edu.jhuapl.trinity.javafx.components;
+
+import javafx.application.Application;
+
+/**
+ * @author Sean Phillips
+ */
+public class CocoViewerMain {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(CocoViewerApp.class, args);
+    }
+}
